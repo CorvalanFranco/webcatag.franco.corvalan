76fc071e71fd77199af3d0e98aa00158fe81dc60
CorvalanFranco <102119156+CorvalanFranco@users.noreply.github.com>
2022-08-18 00:38:38 -0300

    V1.0

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavBar from './components/NavBar.jsx';
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+    <NavBar />
+    </div>
+  );
+}
+
+export default App;
