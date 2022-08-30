cdb1ea2286027dd7578575ceb3f40976691191ea
CorvalanFranco <102119156+CorvalanFranco@users.noreply.github.com>
2022-08-30 02:19:17 -0300

    Add files via upload

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import nike1 from "../assets/buzo.png"
 
-function ItemListContainer() {
+export default function ItemListContainer() {
   return (
-    <div>
-        Acá va el futuro catálogo con card
-    </div>
-  )
+    <Card sx={{ maxWidth: 300 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="200"
+          image={nike1}
+          alt="Nike1"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Hoodie whites
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Buzo blanco full fachero descripción.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Typography
+         size="small" color="primary"
+        >
+        {"$" + 500}
+        </Typography>
+        <Button size="small" color="primary">
+          Añadir carrito
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
-
-export default ItemListContainer
